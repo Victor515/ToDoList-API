1e9e5032fc4e78f0df1f4ef19d2d707337a2a8f8
Victor515 <victorlevian@gmail.com>
2017-11-09 23:43:49 -0600

    final version

--- config/secrets.js
+++ config/secrets.js
@@ -3,5 +3,5 @@
  */
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection : "mongodb://yuboo2:oyyb95515@ds129315.mlab.com:29315/yuboo2db"
+    mongo_connection : "User your MongoDb database account here!"
 };
